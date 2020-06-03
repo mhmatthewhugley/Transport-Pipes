23814969b0af31b85211c3a22a7c005a641ba4ae
BlackBeltPanda <dev@pandacraft.org>
2020-06-03 01:43:19 -0400

    Forgot about Ice Pipes

--- src/main/java/de/robotricker/transportpipes/duct/pipe/IcePipe.java
+++ src/main/java/de/robotricker/transportpipes/duct/pipe/IcePipe.java
@@ -1,6 +1,5 @@
 package de.robotricker.transportpipes.duct.pipe;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,14 +36,20 @@ public class IcePipe extends Pipe {
     
     @Override
     protected Map<TPDirection, Integer> calculateItemDistribution(PipeItem pipeItem, TPDirection movingDir, List<TPDirection> dirs, TransportPipes transportPipes) {
-    	BlockLocation location = getBlockLoc();
-    	List<TPDirection> newDirs = dirs.stream().filter(dir -> pipeItem.hasMovedDirs(location) && !pipeItem.getMovedDirs(location).contains(dir)).collect(Collectors.toList());
-    	if (newDirs.isEmpty()) {
-    		newDirs = pipeItem.hasSourceDir(location) ? Arrays.asList(pipeItem.getSourceDir(location)) : dirs;
-    	}
-		Map<TPDirection, Integer> absWeights = new HashMap<>();
-		newDirs.stream().forEach(dir -> absWeights.put(dir, 1));
-		return itemDistributor.splitPipeItem(pipeItem.getItem(), absWeights, this);
+        BlockLocation location = getBlockLoc();
+        List<TPDirection> newDirs = dirs.stream().filter(dir -> pipeItem.hasMovedDirs(location) && !pipeItem.getMovedDirs(location).contains(dir)).collect(Collectors.toList());
+        if (newDirs.isEmpty()) {
+            newDirs = dirs;
+        }
+        
+        // If we have more than one direction option, make sure we remove the opposite direction to prevent backtracking when possible
+        if (newDirs.contains(movingDir.getOpposite()) && newDirs.size() > 1) {
+            newDirs.remove(movingDir.getOpposite());
+        }
+        
+        Map<TPDirection, Integer> absWeights = new HashMap<>();
+        newDirs.stream().forEach(dir -> absWeights.put(dir, 1));
+        return itemDistributor.splitPipeItem(pipeItem.getItem(), absWeights, this);
 	}
 
 }
